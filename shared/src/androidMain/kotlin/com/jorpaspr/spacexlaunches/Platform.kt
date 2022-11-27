9243cd7fc5186d6ca9a79c82dd4602bd43a25c93
Jorge Pastor <jorpaspr@gmail.com>
2022-11-27 19:06:23 +0000

    Initial commit

---
+++ shared/src/androidMain/kotlin/com/jorpaspr/spacexlaunches/Platform.kt
@@ -0,0 +1,7 @@
+package com.jorpaspr.spacexlaunches
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
