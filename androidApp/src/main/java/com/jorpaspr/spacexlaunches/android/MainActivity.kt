7fc6940f8b17ca351bf2ddcee6956a1f4d2dfd36
Jorge Pastor <jorpaspr@gmail.com>
2022-12-04 21:53:56 +0000

    Move launches handling to view model

--- androidApp/src/main/java/com/jorpaspr/spacexlaunches/android/MainActivity.kt
+++ androidApp/src/main/java/com/jorpaspr/spacexlaunches/android/MainActivity.kt
@@ -5,25 +5,16 @@ import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.*
 import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.isVisible
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.themeadapter.material.MdcTheme
-import com.jorpaspr.spacexlaunches.SpaceXSdk
-import com.jorpaspr.spacexlaunches.cache.DatabaseDriverFactory
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val progressBarView: FrameLayout by lazy { findViewById(R.id.progressBar) }
 
-    private val sdk = SpaceXSdk(DatabaseDriverFactory(this))
-
     private val composeView: ComposeView by lazy { findViewById(R.id.composeView) }
 
-    private val viewModel: MainViewModel by viewModels { MainViewModelFactory() }
+    private val viewModel: MainViewModel by viewModels { MainViewModel.Factory(application) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,38 +23,16 @@ class MainActivity : AppCompatActivity() {
 
         composeView.setContent {
             MdcTheme {
-                LaunchList()
+                LaunchList(viewModel)
             }
         }
 
-        viewModel.refreshing.observe(this) { refreshing ->
-            if (refreshing) {
-                displayLaunches(true)
-            }
+        viewModel.loading.observe(this) { loading ->
+            progressBarView.isVisible = loading
         }
 
-        displayLaunches(false)
-    }
-
-    private fun displayLaunches(needReload: Boolean) {
-        progressBarView.isVisible = !needReload
-        lifecycleScope.launch {
-            runCatching {
-                sdk.getLaunches(needReload)
-            }.onSuccess {
-                viewModel.launches.value = it
-            }.onFailure {
-                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
-            }
-            progressBarView.isVisible = false
-            viewModel.refreshing.value = false
+        viewModel.error.observe(this) { message ->
+            Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
         }
-    }
-}
-
-class MainViewModelFactory : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return MainViewModel() as T
     }
 }
--- androidApp/src/main/java/com/jorpaspr/spacexlaunches/android/MainViewModel.kt
+++ androidApp/src/main/java/com/jorpaspr/spacexlaunches/android/MainViewModel.kt
@@ -1,15 +1,58 @@
 package com.jorpaspr.spacexlaunches.android
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import com.jorpaspr.spacexlaunches.SpaceXSdk
+import com.jorpaspr.spacexlaunches.android.livedata.SingleLiveEvent
+import com.jorpaspr.spacexlaunches.cache.DatabaseDriverFactory
 import com.jorpaspr.spacexlaunches.entity.RocketLaunch
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-    var launches = MutableLiveData<List<RocketLaunch>>(emptyList())
+class MainViewModel(
+    private val sdk: SpaceXSdk
+) : ViewModel() {
+    private val _launches = MutableLiveData<List<RocketLaunch>>(emptyList())
+    val launches: LiveData<List<RocketLaunch>> = _launches
 
-    var refreshing = MutableLiveData(false)
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> = _loading
+
+    private val _refreshing = MutableLiveData(false)
+    val refreshing: MutableLiveData<Boolean> = _refreshing
+
+    private val _error = SingleLiveEvent<String>()
+    val error: LiveData<String> = _error
+
+    init {
+        _loading.value = true
+        displayLaunches(needReload = false)
+    }
+
+    private fun displayLaunches(needReload: Boolean) {
+        viewModelScope.launch {
+            runCatching {
+                sdk.getLaunches(forceReload = needReload)
+            }.onSuccess {
+                _launches.value = it
+            }.onFailure {
+                _error.value = it.localizedMessage
+            }
+            _loading.value = false
+            refreshing.value = false
+        }
+    }
 
     fun refresh() {
         refreshing.value = true
+        displayLaunches(needReload = true)
+    }
+
+    class Factory(
+        private val application: Application
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MainViewModel(SpaceXSdk(DatabaseDriverFactory(application))) as T
+        }
     }
 }
